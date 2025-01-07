ece096f6c83b3ca006f98725a7cedd4cdc17107e
alamin72b <alamin72b@gmail.com>
2025-01-07 23:34:12 +0600

    List_view Functionality added

--- login/views.py
+++ login/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
 
 # Create your views here.
-def index(request):
-    return render(request, 'login/base.html')
+# def index(request):
+#     return render(request, 'login/base.html')
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'tasks'
