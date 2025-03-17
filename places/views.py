684f76c3839c7f87eed00f0e4d6ac84082f3f1a5
alamin72b <alamin72b@gmail.com>
2025-03-17 12:38:31 +0600

    Added initial project files

--- places/views.py
+++ places/views.py
@@ -3,6 +3,8 @@ from .models import Place, Review
 from .forms import ReviewForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from .models import Place
+
 
 @login_required
 def add_review(request, place_id):
@@ -26,10 +28,8 @@ def home(request):
     reviews = Review.objects.all()[:5]  # Fetch the latest 5 reviews
     return render(request, 'places/home.html', {'places': places, 'reviews': reviews})
 
-# places/views.py
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Place
-from .forms import ReviewForm
+
+
 
 def place_detail(request, place_id):
     place = get_object_or_404(Place, id=place_id)
@@ -66,28 +66,3 @@ def search_view(request):
     })
 
 
-def place_detail(request, place_id):
-    place = get_object_or_404(Place, id=place_id)
-    place_reviews = place.reviews.all()
-    
-    if request.method == 'POST':
-        if request.user.is_authenticated:
-            review_form = ReviewForm(request.POST)
-            if review_form.is_valid():
-                new_review = review_form.save(commit=False)
-                new_review.place = place
-                new_review.user = request.user
-                new_review.name = request.user.username
-                new_review.save()
-                return redirect('place_detail', place_id=place.id)
-        else:
-            return redirect('login')
-    else:
-        review_form = ReviewForm()
-
-    context = {
-        'place': place,
-        'reviews': place_reviews,
-        'review_form': review_form,
-    }
-    return render(request, 'places/place_detail.html', context)
--- static/js/login.js
+++ static/js/login.js
@@ -2,16 +2,11 @@ function togglePassword() {
     const passwordInput = document.getElementById('login-password');
     const toggleIcon = document.getElementById('toggle-password');
     
-    // Check if the current type of the password input field is 'password'
     if (passwordInput.type === 'password') {
-        // If the password is currently hidden, change the type to 'text' to show it
-        passwordInput.type = 'text';
-        // Change the icon to indicate that the password is now visible
-        toggleIcon.className = 'ri-eye-off-line login__eye';
+      passwordInput.type = 'text';
+      toggleIcon.className = 'ri-eye-off-line login__eye';
     } else {
-        // If the password is currently visible, change the type back to 'password' to hide it
-        passwordInput.type = 'password';
-        // Change the icon to indicate that the password is now hidden
-        toggleIcon.className = 'ri-eye-line login__eye';
+      passwordInput.type = 'password';
+      toggleIcon.className = 'ri-eye-line login__eye';
     }
-}
+  }
